f8a14fc9a83d9729907c624018b55ffa623c9cbf
Hitb23 <hit.bambhroliya@etatvasoft.com>
2024-06-12 11:42:02 +0530

    client added

---
+++ client/src/assets/index.js
@@ -0,0 +1,15 @@
+import Facebook from './facebook-icon.svg';
+import LinkedIn from './linkedin-icon.svg';
+import Youtube from './youtube-icon.svg';
+import Instagram from './instagram-icon.svg';
+import WelcomePoster from './welcome-poster.png';
+import Logo from './logo-icon.svg';
+
+export {
+  Facebook,
+  LinkedIn,
+  Youtube,
+  Instagram,
+  WelcomePoster,
+  Logo
+};
---
+++ client/src/pages/WelcomePage.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import LandingHeader from "../components/headers/LandingHeader";
+import LandingFooter from "../components/footers/LandingFooter";
+import classes from "./WelcomePage.module.css";
+import { WelcomePoster } from "../assets";
+import { Button } from "bootstrap";
+import { Link } from "react-router-dom";
+ 
+function WelcomePage() {
+  return (
+    <React.Fragment>
+      <LandingHeader />
+      <main className={classes["main-component"]}>
+        <div className={classes["welcome-message"]}>
+          <div className={classes["welcome-title"]}>Welcome To QuizWhiz</div>
+          <div className={classes["welcome-slogan"]}>
+            Where Every Game is a Brain Boost!
+          </div>
+        </div>
+        <div className={classes["poster"]}>
+          <img
+            className={`img-responsive ${classes["poster-img"]}`}
+            src={WelcomePoster}
+            alt="logo"
+          />
+          <Link to="">
+            <button className={classes["get-started-button"]}>Get Started</button>
+          </Link>
+        </div>
+      </main>
+      <LandingFooter />
+    </React.Fragment>
+  );
+}
+
+export default WelcomePage;
