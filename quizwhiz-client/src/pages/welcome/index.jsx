24439abadcfae9725aab92e30cd52acc0d719607
Ishan Bhatt <ishan.bhatt@etatvasoft.com>
2024-06-20 15:06:44 +0530

    changes in landing page

--- quizwhiz-client/src/pages/welcome/index.jsx
+++ quizwhiz-client/src/pages/welcome/index.jsx
@@ -7,14 +7,37 @@ import { WelcomePoster } from "../../assets";
 import { Button } from "bootstrap";
 import { Link } from "react-router-dom";
 import { RoutePaths } from "../../utils/enum";
-
+import { LandingImage } from "../../assets";
+import Typed from 'typed.js';
 const Welcome =  () => {
+  const el = React.useRef(null);
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['Get Your Brain in Gear','Welcome to QuizWhiz'],
+      typeSpeed: 50,
+      backSpeed: 50,
+      backDelay: 1000,
+      startDelay: 500,
+      loop: false, // Set loop to false to stop after typing is complete
+      onComplete: (self) => {
+        if (el.current) {
+          // Remove the cursor class after typing is complete
+          el.current.querySelector('.typed-cursor').style.display = 'none';
+        }
+      },
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
   return (
     <React.Fragment>
       <LandingHeader />
       <main className={classes["main-component"]}>
-        <div className={classes["welcome-message"]}>
-          <div className={classes["welcome-title"]}>Welcome To QuizWhiz</div>
+        <div className={`text-center`}>
+          <div className={classes["welcome-title"]}> <span ref={el} /></div>
           <div className={classes["welcome-slogan"]}>
             Where Every Game is a Brain Boost!
           </div>
@@ -22,12 +45,9 @@ const Welcome =  () => {
         <div className={classes["poster"]}>
           <img
             className={`img-responsive ${classes["poster-img"]}`}
-            src={WelcomePoster}
+            src={LandingImage}
             alt="logo"
           />
-          <Link to={RoutePaths.Welcome}>
-            <button className={classes["get-started-button"]}>Get Started</button>
-          </Link>
         </div>
       </main>
       <LandingFooter />
