6aa6983b3cf54fd71f58cb7cad2ae9554e910fb7
pci20 <pci20@PCI20>
2024-08-14 11:02:40 +0530

    Final Changes

--- quizwhiz-client/src/pages/buy-lifelines/index.jsx
+++ quizwhiz-client/src/pages/buy-lifelines/index.jsx
@@ -1,85 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import { HubConnectionBuilder } from "@microsoft/signalr";
-// import jwtDecoder from "../../services/jwtDecoder";
-// import useWindowSize from 'react-use/lib/useWindowSize'
-// import Confetti from 'react-confetti'
-// import { driver } from "driver.js";
-// import "driver.js/dist/driver.css";
-
-// const Quiz = () => {
-//   const [minutes, SetMinutes] = useState(0);
-//   const [Seconds, SetSeconds] = useState(0);
-//   const [newMessage, setNewMessage] = useState("");
-//   const [Question,SetQuestion]=useState("");
-//   const [connection, setConnection] = useState(null);
-//   const [isContestActive, setIsContestActive] = useState(false);
-//   const [remainingTime, setRemainingTime] = useState(null);
-//   const [showJoinButton, setShowJoinButton] = useState(false);
-//   const [questiontext,SetQuestiontext]=useState('');
-//   const formatTime = (time) => {
-//     const seconds = String(time.seconds).padStart(2, "0");
-//     const minutes = String(time.minutes).padStart(2, "0");
-//     const hours = String(time.hours).padStart(2, "0");
-//     return `${hours}:${minutes}:${seconds}`;
-//   };
-
-//    useEffect(() => {
-//     const connection = new HubConnectionBuilder()
-//       .withUrl("https://localhost:44361/quizhub")
-//       .withAutomaticReconnect()
-//       .build();
-//     setConnection(connection);
-
-//     connection.on("ReceiveRemainingTime_PEPa86wz", (minutes, seconds) => {
-//       connection
-//       .invoke("SendAnswer", user,4)
-//       .catch((error) => console.error("SendMessage failed: ", error));
-//       console.log(minutes + "-" + seconds);
-//       SetMinutes(minutes);
-//       SetSeconds(seconds)
-//     });
-//     connection.on("ReceiveQuestion_PEPa86wz", (questionNo,Question,seconds) => {
-//       console.log("QuestionNo : "+ questionNo +" : " + Question)
-//       SetQuestion(Question);
-//       // SetSeconds(seconds)
-//     });
-//     connection.on("ReceiveTimerSeconds_PEPa86wz", (seconds) => {
-//       console.log("TimerSeconds : "+ seconds)
-//       // SetSeconds(seconds)
-//     });
-//     connection.on("ReceiveAnswer_PEPa86wz", (questionNo,ans,seconds) => {
-//       console.log("questionNo : "+questionNo +" : "+"Answer : "+ans)
-//       // console.log(ans);
-//       // SetSeconds(seconds)
-//     });
-
-//     console.log(connection);
-//       connection
-//         .start()
-//         .catch((error) => console.error("Connection failed: ", error));
-//       return () => connection.stop();
-//   }, [Seconds,Question]);
-
-//   const fun=async()=> {
-//     console.log(connection)
-//     if(connection){
-//       await connection.invoke("UpdateScore",'WJqpRNMN','ishanbhatt',59,[1]);
-//     }
-//   }
-
-//   return (
-//   //   <div>
-//   //      <Confetti
-//   //      className="h-100 w-100"
-//   //     />
-//   //     <h1>Quiz Whiz</h1>
-//   //     <button onClick={fun}>Send Message</button>
-//   //     <button>Open </button>
-//   //   </div>
-//   // )
-// };
-
-// export default Quiz;
 import React, { useEffect, useState } from "react";
 import { styled } from "@mui/material/styles";
 import Dialog from "@mui/material/Dialog";
@@ -96,7 +14,7 @@ import {
   LifeLineBG,
   SkipPY,
   FiftyPY,
-  HeartPY
+  HeartPY,
 } from "../../assets/index";
 import withReactContent from "sweetalert2-react-content";
 import classes from "./style.module.css";
@@ -118,15 +36,9 @@ const BuyLifelines = ({ isOpen, closeHandler, coinsAndLifelinesDetails }) => {
   const [lifeLines, setLifeLines] = useState(
     coinsAndLifelinesDetails?.Lifelines
   );
-  const [skipLifeline, setSkipLifeline] = useState(
-    0
-  );
-  const [anotherChanceLifeline, setAnotherChanceLifeline] = useState(
-    0
-  );
-  const [fiftyLifeline, setFiftyLifeline] = useState(
-    0
-  );
+  const [skipLifeline, setSkipLifeline] = useState(0);
+  const [anotherChanceLifeline, setAnotherChanceLifeline] = useState(0);
+  const [fiftyLifeline, setFiftyLifeline] = useState(0);
   const MySwal = withReactContent(Swal);
   const NotEnoughCoinsHandler = async () => {
     await Swal.fire({
@@ -144,9 +56,11 @@ const BuyLifelines = ({ isOpen, closeHandler, coinsAndLifelinesDetails }) => {
     setOpen(isOpen ?? false);
     setCoins(coinsAndLifelinesDetails?.CoinsCount ?? 0);
     setLifeLines(coinsAndLifelinesDetails?.Lifelines);
-    setSkipLifeline(coinsAndLifelinesDetails?.UserLifelines[0]);
-    setFiftyLifeline(coinsAndLifelinesDetails?.UserLifelines[1]);
-    setAnotherChanceLifeline(coinsAndLifelinesDetails?.UserLifelines[2]);
+    if (coinsAndLifelinesDetails?.UserLifelines) {
+      setSkipLifeline(coinsAndLifelinesDetails?.UserLifelines[0]);
+      setFiftyLifeline(coinsAndLifelinesDetails?.UserLifelines[1]);
+      setAnotherChanceLifeline(coinsAndLifelinesDetails?.UserLifelines[2]);
+    }
   }, [coinsAndLifelinesDetails]);
 
   const handleClickOpen = () => {
@@ -166,21 +80,17 @@ const BuyLifelines = ({ isOpen, closeHandler, coinsAndLifelinesDetails }) => {
     try {
       const response = await BuyLifeline(data);
       // console.log(response);
-       if (id == 1){
-         setSkipLifeline({ LifelineCount: count + 1, LifelineId: id });
-         setCoins((coins)=> Math.max(coins-150,0))
-       }
-       else if (id == 2){
-         setFiftyLifeline({ LifelineCount: count + 1, LifelineId: id });
-         setCoins((coins)=> Math.max(coins-100,0))
-        }
-        else if (id == 3){
-         setAnotherChanceLifeline({ LifelineCount: count + 1, LifelineId: id });
-         setCoins((coins)=> Math.max(coins-200,0))
-        }
-       
-    } 
-    catch (error) {
+      if (id == 1) {
+        setSkipLifeline({ LifelineCount: count + 1, LifelineId: id });
+        setCoins((coins) => Math.max(coins - 150, 0));
+      } else if (id == 2) {
+        setFiftyLifeline({ LifelineCount: count + 1, LifelineId: id });
+        setCoins((coins) => Math.max(coins - 100, 0));
+      } else if (id == 3) {
+        setAnotherChanceLifeline({ LifelineCount: count + 1, LifelineId: id });
+        setCoins((coins) => Math.max(coins - 200, 0));
+      }
+    } catch (error) {
       if (error.response.data.isSuccess == false) NotEnoughCoinsHandler();
     }
   };
@@ -222,7 +132,7 @@ const BuyLifelines = ({ isOpen, closeHandler, coinsAndLifelinesDetails }) => {
         className="shadow-lg"
       >
         <DialogTitle
-          sx={{ background: "#3d3189" ,minWidth:'5rem'}}
+          sx={{ background: "#3d3189", minWidth: "5rem" }}
           id="customized-dialog-title"
         >
           <div className="d-flex justify-content-between align-items-center flex-wrap">
@@ -230,7 +140,11 @@ const BuyLifelines = ({ isOpen, closeHandler, coinsAndLifelinesDetails }) => {
               <h2 className={` ${classes["Shoptext"]} text-black mt-2 `}>
                 SHOP
               </h2>
-              <img src={Knapsack} height={65}  className={`${classes['knapsack-design']} `}/>
+              <img
+                src={Knapsack}
+                height={65}
+                className={`${classes["knapsack-design"]} `}
+              />
             </div>
 
             {/* <div
@@ -249,7 +163,7 @@ const BuyLifelines = ({ isOpen, closeHandler, coinsAndLifelinesDetails }) => {
               className={`rounded-4 d-flex  justify-content-between align-items-center p-2 ${classes["lifeLineShadow"]}`}
               style={{ background: "#6F41DB" }}
             >
-              <img src={CoinIcon} height={28}  />
+              <img src={CoinIcon} height={28} />
               <small className={`mx-1  fw-bold  ${classes["text-bg"]}`}>
                 {coins}
               </small>
